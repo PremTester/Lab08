bbde20687eedafe8c43abc8ed3aab7fb18d887ba
Jaratrawee <iceonepiece@gmail.com>
2014-03-13 20:39:51 +0700

    draw

--- Lab8/Lab8/Freehand_Drawing.py
+++ Lab8/Lab8/Freehand_Drawing.py
@@ -5,30 +5,82 @@ from PySide.QtGui import *
 class Freehand_window(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
+
+
         self.setWindowTitle("A simple paint program")
 
         layout = QVBoxLayout(self)
 
-        self.clear_button = QPushButton('Clear')
-        self.clear_button.clicked.connect(self.Clear_Screen)
-        self.clear_button.show()
+     
         self.Label = QLabel("Drag the mouse to draw")
+        self.clear_button = QPushButton("Clear")
+        self.clear_button.clicked.connect( self.clear_image )
+        self.animation_area = Animation_area()
+
+        layout.addWidget(self.animation_area)
+        
         layout.addWidget(self.Label)
-        layout.addWidget( self.clear_button )
+        layout.addWidget(self.clear_button)
 
         self.setLayout( layout )
 
-   
+    def clear_image(self):
+        self.animation_area.list.clear()
 
-    def Clear_Screen(self):
-        print('Clear')
 
-    def mousePressEvent(self, event):
-        print('Press')
 
-    def mouseReleaseEvent(self, event):
-        print('Release')
-      
+class Animation_area(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.list = []
+
+
+        self.setMinimumSize(500, 500)
+
+        self.arena_w = 500
+        self.arena_h = 500
+     
+
+        timer = QTimer(self)
+        self.connect(timer, SIGNAL("timeout()"), self.update_value)
+        timer.start(500)
+
+    def mousePressEvent(self, e):
+        
+        
+        self.list.append( Dot(e.pos().x(), e.pos().y()) )
+        print("Press")
+
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+        p.setPen( QColor(0,0,0))
+        
+        for i in self.list:
+            p.drawPolygon([
+                           QPoint( i.x , i.y ), QPoint( i.x + 5, i.y),
+                           QPoint( i.x, i.y + 5), QPoint( i.x, i.y + 5),
+                            ])
+
+
+        p.end()
+
+    def mouseMoveEvent(self, e):
+        self.list.append( Dot(e.pos().x(), e.pos().y()) )
+
+
+    def update_value(self):
+    
+        self.update()
+
+class Dot():
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+ 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
