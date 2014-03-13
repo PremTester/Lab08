09806467cbe70a641e36ccda60728d2e8547cc99
PremTester <enjoy_ing16@hotmail.com>
2014-03-13 20:02:59 +0700

    Button

--- Lab8/Lab8/Freehand_Drawing.py
+++ Lab8/Lab8/Freehand_Drawing.py
@@ -7,6 +7,13 @@ class Freehand_window(QWidget):
         QWidget.__init__(self, None)
         self.setWindowTitle("A simple paint program")
 
+        self.clear_button = QPushButton('Clear')
+        self.clear_button.clicked.connect(Clear_Screen)
+
+    def Clear_Screen(self):
+        print('Clear')
+      
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = Freehand_window()
