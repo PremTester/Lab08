a2b746a4431d00cbaefe43b9f36474ff97875ea0
BoyKananek <kananek.atichatwiwat@hotmail.com>
2014-03-13 19:55:53 +0700

    Fix_simple_window

--- Lab8/Lab8/Freehand_Drawing.py
+++ Lab8/Lab8/Freehand_Drawing.py
@@ -6,10 +6,6 @@ class Freehand_window(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Freehand_Drawing")
-        self.setMinimumHeight(100)
-        self.setMinimumWidth(250)
-        self.setMaximumHeight(200)
-        self.setMaximumWidth(800)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
