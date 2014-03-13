4ee19dcff3f715c871dd9ae64d866661d623e047
Jaratrawee <iceonepiece@gmail.com>
2014-03-13 19:33:29 +0700

    kkk

--- Lab8/Lab8/Lab8.py
+++ Lab8/Lab8/Lab8.py
@@ -50,30 +50,17 @@ class Simple_drawing_window2(Simple_drawing_window):
         p.end()
 
 
-class Simple_drawing_window3(QWidget):
+class Simple_drawing_window3(Simple_drawing_window):
     def __init__(self):
-        QWidget.__init__(self, None)
-        self.setWindowTitle("Simple Drawing")
-        self.rabbit = QImage("images/rabbit.png")
+        Simple_drawing_window.__init__(self)
+        self.setWindowTitle("Simple Drawing 3")
 
     def paintEvent(self, e):
         p = QPainter()
         p.begin(self)
-
         p.setPen(QColor(0, 0, 0))
-        p.setBrush(QColor(0, 127, 0))
-        p.drawPolygon([
-                       QPoint(70, 100), QPoint(100, 110),
-                       QPoint(130, 100), QPoint(100, 150),
-                       ])
-        
-        p.setPen(QColor(255, 127, 0))
-        p.setBrush(QColor(255, 127, 0))
-        p.drawPie(50, 150, 100, 100, 0, 180 * 16)
-
-        p.drawPolygon([
-                       QPoint(50, 200), QPoint(150, 200), QPoint(100, 400),
-                       ])
+        p.setBrush(QColor(30, 30, 127))
+        p.drawPie(50, 150, 100, 300, 30, 180 * 16)
 
         p.drawImage(QRect(200, 100, 320, 320), self.rabbit)
         p.end()
@@ -85,5 +72,7 @@ if __name__ == '__main__':
     window1.show()
     window2 = Simple_drawing_window2()
     window2.show()
+    window3 = Simple_drawing_window3()
+    window3.show()
     app.exec_()
     sys.exit(0)
