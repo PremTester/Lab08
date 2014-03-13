4d94a1918d537391cc7eacfddfd1dc8fee0e145e
BoyKananek <kananek.atichatwiwat@hotmail.com>
2014-03-13 19:43:56 +0700

    detComment

--- Lab8/Lab8/Freehand_Drawing.py
+++ Lab8/Lab8/Freehand_Drawing.py
@@ -6,11 +6,5 @@ from PySide.QtGui import *
 if __name__ == '__main__':
  
     app = QApplication(sys.argv)
-    #window1 = Simple_drawing_window()
-    #window1.show()
-    #window2 = Simple_drawing_window2()
-    #window2.show()
-    #window3 = Simple_drawing_window3()
-    #window3.show()
     app.exec_()
     sys.exit(0)
