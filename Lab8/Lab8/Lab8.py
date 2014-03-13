ad64accb3a3ecc79571e7bcbfc9085a72f74b6f4
BoyKananek <kananek.atichatwiwat@hotmail.com>
2014-03-13 18:26:53 +0700

    Change

--- Lab8/Lab8/Lab8.py
+++ Lab8/Lab8/Lab8.py
@@ -1,1 +1,8 @@
-print('Hello World')
+import sys
+from PySide.QtCore import *
+from PySide.QtGui import *
+
+class Simple_drawing_window(QWidget):
+    def __init__(self):
+        QWidget.__init__(self,None)
+        self.setWindowTitle("Simple Drawing")
